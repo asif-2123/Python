5a80a3ab3aff0ff49e86237c0499971e7b1c5015
Md Asif <asif212327@gmail.com>
2026-03-29 10:26:37 +0530

    Add files via upload

---
+++ Python/List_Tuples/List_creating.py
@@ -0,0 +1,8 @@
+a=[]
+mov1=input("Enter your favourite movie: ")
+mov2=input("Enter your second favourite movie: ")
+mov3=input("Enter your third favourite movie: ")
+a.append(mov1)
+a.append(mov2)
+a.append(mov3)
+print(a)
