325e61bb92feec4c3d84d05615c8fab55b3344f7
Md Asif <asif212327@gmail.com>
2026-04-01 17:19:45 +0530

    Add files via upload

---
+++ Python/Functions_Recursion/Element_prints.py
@@ -0,0 +1,11 @@
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+heroes = ["Spider-Man", "Batman", "Superman", "Wonder Woman", "The Flash"]
+
+def element(list):
+    for i in list:
+        print(i, end=" ")
+
+element(cities)
+print()
+element(heroes)
+print()
