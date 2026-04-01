31bf7c2901fd660da5f35437b012593146c74ab8
Md Asif <asif212327@gmail.com>
2026-04-02 00:39:13 +0530

    Add files via upload

---
+++ Python/Functions_Recursion/Length.py
@@ -0,0 +1,8 @@
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+heroes = ["Spider-Man", "Batman", "Superman", "Wonder Woman", "The Flash"]
+
+def length(list):
+    print(len(list))
+
+length(cities)
+length(heroes)
