278036236f022696b68e0d7dbbc0b413ce31f655
Md Asif <asif212327@gmail.com>
2026-04-03 19:34:41 +0530

    Delete Python/Print_list.py

--- Python/Print_list.py
+++
@@ -1,9 +0,0 @@
-list=["a","b","c","d","e"]
-
-def print_list(list,index=0):
-    if index==len(list):
-        return
-    print(list[index],end=" ")
-    print_list(list,index+1)
-
-print_list(list)
