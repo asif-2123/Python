441d1fc65206b6667403e8ea2e98189d9947aedd
Md Asif <asif212327@gmail.com>
2026-05-05 10:46:34 +0530

    Delete Python/Loops/Matplotlib directory

--- Python/Loops/Matplotlib/Annotation.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-time = [0, 1, 2, 3, 4, 5]
-temp = [30, 32, 34, 35, 33, 31]
-plt.plot(time, temp)
-plt.annotate('Peak', xy=(3,35) , xytext=(4, 36), arrowprops=dict(facecolor='black', shrink=0.05))
-plt.show()
