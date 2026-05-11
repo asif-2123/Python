8bc2fa4e953eb23ffe7fe0dca1e7f0b3a749a636
Md Asif <asif212327@gmail.com>
2026-05-11 13:43:26 +0530

    Add files via upload

---
+++ Python/Numpy/Linspace.py
@@ -0,0 +1,6 @@
+import numpy as np 
+
+print(np.linspace(0, 1, 5))# 1-D array of 5 evenly spaced numbers from 0 to 1
+
+print(np.linspace(2,11,17))
+#[start, stop, num = this means in how many numbers we want to divide the range]
