442bdd24452cf991180feb38aa6be5bf5d7e3721
Md Asif <asif212327@gmail.com>
2026-05-13 21:38:14 +0530

    Add files via upload

---
+++ Python/Numpy/Mathematics.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+arr1=([[1,2,3,4,5],[6,7,8,9,10]])
+
+print(np.sum(arr1))    # sum of all elements
+print(np.mean(arr1))   # mean of all elements
+print(np.median(arr1)) # median of all elements
+print(np.max(arr1))    # maximum of all elements
+print(np.min(arr1))  # minimum of all elements
