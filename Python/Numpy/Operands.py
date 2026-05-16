c5c03117be011da2587a84a657433331885e0ed1
Md Asif <asif212327@gmail.com>
2026-05-16 12:36:13 +0530

    Add files via upload

---
+++ Python/Numpy/Operands.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr1 = np.array([1, 4, 5])
+arr2 = np.array([1, 2, 3])
+
+sum = arr1 + arr2
+subtraction = arr1 - arr2
+multiplication = arr1 * arr2
+division = arr1 / arr2
+
+print("Sum:", sum)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
