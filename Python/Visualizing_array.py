f7eb528127b466a06bbec695299a4f74cfff88b3
Md Asif <asif212327@gmail.com>
2026-05-20 00:35:25 +0530

    Delete Python/Visualizing_array.py

--- Python/Visualizing_array.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-arr = np.array([[1, 2, 3], [4, 5, 6]])
-print(arr.shape) #(2, 3) means 2 rows and 3 columns
-print(arr.ndim)  # number of dimensions
-print(arr.size)  # total number of elements
-print(arr.dtype) # data type of elements
