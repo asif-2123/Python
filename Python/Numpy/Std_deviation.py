9e7f1c7d9ec82bb3f7b4316ee54a0921269bcda4
Md Asif <asif212327@gmail.com>
2026-05-21 20:05:42 +0530

    Delete Python/Numpy/Std_deviation.py

--- Python/Numpy/Std_deviation.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-arrr = np.random.rand(10)
-
-print(arrr) # original array
-print(np.std(arrr)) # standard deviation of the array
-print(np.mean(arrr)) # mean of the array
-print(np.median(arrr)) # median of the array
