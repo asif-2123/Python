5861c0830929608d2ad91bbf91e0426eab6be21a
Md Asif <asif212327@gmail.com>
2026-05-21 20:06:25 +0530

    Add files via upload

---
+++ Python/Numpy/Std_deviation.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arrr = np.random.rand(10)
+
+print(arrr) # original array
+print(np.std(arrr)) # standard deviation of the array
+print(np.mean(arrr)) # mean of the array
+print(np.median(arrr)) # median of the array
